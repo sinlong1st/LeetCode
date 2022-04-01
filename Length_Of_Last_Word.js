e25644311e98cffd813ca81ff708695cf1afbad2
sinlong1st <sinlong1st@gmail.com>
2022-04-01 02:09:25 -0700

    Length of last word in the string - 1 loop but not fast

---
+++ Length_Of_Last_Word.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var lengthOfLastWord = function(s) {
+    let spaceNum = 0;
+    for (let i = s.length - 1; i >= 0; i--){
+        // if ((s[i] < 32) || (s[i] > 32 && s[i] < 65) || (s[i] > 90 && s[i] < 97) || (s[i] > 122)){
+        //     return 0;
+        //     break;
+        // }
+        if (s[i] == " ")
+            spaceNum++;
+        if (s[i] != " "){
+            if (i == 0){
+                return (s.length - i - spaceNum);
+            }else{
+                if (s[i-1] == " ")
+                    return (s.length - i - spaceNum);
+            }
+        }    
+    }
+    return 0;
+};
