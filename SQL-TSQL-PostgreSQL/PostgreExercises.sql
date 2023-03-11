556007d6e71f74999e8f19d1ab94ecd8153f455e
sinlong1st <32693869+sinlong1st@users.noreply.github.com>
2023-03-10 23:04:42 -0800

    polish e5 exercise, subquery, simplify e5

--- SQL-TSQL-PostgreSQL/PostgreExercises.sql
+++ SQL-TSQL-PostgreSQL/PostgreExercises.sql
@@ -47,3 +47,20 @@ SELECT distinct concat(a.firstname,' ',a.surname) as member,
 		 where b.memid = a.recommendedby) as recommender
 from cd.members a
 order by member
+
+The Produce a list of costly bookings exercise contained some messy logic: we had to calculate the booking cost in both the WHERE clause and the CASE statement. 
+Try to simplify this calculation using subqueries. For reference, the question was:
+How can you produce a list of bookings on the day of 2012-09-14 which will cost the member (or guest) more than $30? 
+Remember that guests have different costs to members (the listed costs are per half-hour 'slot'), and the guest user is always ID 0. 
+Include in your output the name of the facility, the name of the member formatted as a single column, and the cost. Order by descending cost.
+select member, facility, cost from 
+(select a.starttime,concat(b.firstname,' ',b.surname) as member, 
+c.name as facility,
+case
+	when a.memid = 0 then a.slots*c.guestcost
+	else a.slots*c.membercost
+end as cost
+from cd.bookings a JOIN cd.members b on a.memid = b.memid
+JOIN cd.facilities c on a.facid = c.facid) as foo
+where date(starttime) = '2012-09-14' and cost > 30
+order by cost desc
