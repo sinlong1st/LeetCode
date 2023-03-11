a349583348dc66a9dcaf687c821009c536c740d2
sinlong1st <32693869+sinlong1st@users.noreply.github.com>
2023-03-10 19:30:44 -0800

    Add threeJoin, switchcase e5

--- SQL-TSQL-PostgreSQL/PostgreExercises.sql
+++ SQL-TSQL-PostgreSQL/PostgreExercises.sql
@@ -23,3 +23,18 @@ from cd.bookings b join cd.members a  on a.memid = b.memid
 join cd.facilities c on b.facid = c.facid
 where c.name like 'Tennis%'
 order by member, c.name
+
+How can you produce a list of bookings on the day of 2012-09-14 which will cost the member (or guest) more than $30? 
+Remember that guests have different costs to members (the listed costs are per half-hour 'slot'), and the guest user is always ID 0. 
+Include in your output the name of the facility, the name of the member formatted as a single column, and the cost. Order by descending cost, and do not use any subqueries.
+select concat(b.firstname,' ',b.surname) as member, 
+c.name as facilitiy,
+case
+	when a.memid = 0 then a.slots*c.guestcost
+	else a.slots*c.membercost
+end as cost
+from cd.bookings a JOIN cd.members b on a.memid = b.memid
+JOIN cd.facilities c on a.facid = c.facid
+where date(starttime) = '2012-09-14' 
+and ((a.memid = 0 and a.slots*c.guestcost > 30) or (a.memid <> 0 and a.slots*c.membercost > 30))
+order by cost desc
